eb11e74ca9a274335e081ffe04d7aa5a085419a9
Yuulis <yuulis2005@gmail.com>
2022-01-07 14:58:33 +0900

    Ver. 1.0の開発完了。

--- LifeGame/Main.cpp
+++ LifeGame/Main.cpp
@@ -5,8 +5,7 @@ using namespace SimpleGUI;
 using namespace Window;
 using namespace Palette;
 
-
-// 1 cell => 1bit
+// 1 cell => 1byte
 struct Cell
 {
 	bool previous : 1 = 0;
@@ -19,7 +18,7 @@ void RandomInit(Grid<Cell>& world, double rate) {
 	for (auto y : Range(1, world.height() - 2))
 	{
 		for (auto x : Range(1, world.width() - 2)) {
-			world[y][x] = Cell{0, RandomBool(rate)};
+			world[y][x] = Cell{ 0, RandomBool(rate) };
 		}
 	}
 }
@@ -57,8 +56,8 @@ void UpdateWorld(Grid<Cell>& world, int32 gameRule) {
 			* 4 : B3678/S34678 (Day and Night)
 			* 5 : B1357/S1357 (Replicator)
 			* 6 : B1357/S02468 (Replicator2 / Fredkin)
-			* 7 : B36/S125 (2x2) 
-			* 
+			* 7 : B36/S125 (2x2)
+			*
 			*/
 
 			if (gameRule == 1) {
@@ -145,7 +144,6 @@ void CopyToImg(const Grid<Cell>& world, Image& img) {
 	for (auto y : step(img.height())) {
 		for (auto x : step(img.width())) {
 			img[y][x] = world[y + 1][x + 1].current ? Color{ 0, 255, 0 } : Black;
-
 		}
 	}
 }
@@ -154,11 +152,11 @@ void Main() {
 	// JSON
 	const JSON settings = JSON::Load(U"config/settings.json");
 	if (not settings) {
-		throw Error{U"Failed to load `settings.json`."};
+		throw Error{ U"Failed to load `settings.json`." };
 	}
 
 	// ===== JSON Input =====
-	// World size
+	// World size (#Default width : 100, height : 70)
 	const int32 width = settings[U"WorldSize"][U"width"].get<int32>();
 	const int32 height = settings[U"WorldSize"][U"height"].get<int32>();
 
@@ -171,7 +169,6 @@ void Main() {
 	// ======================
 
 	// Game rule
-	// const int32 gameRule = settings[U"GameRule"].get<int32>();
 	size_t gameRuleIndex = 0;
 	const Array<String> textOptions = { U"1 : B3/S23",
 										U"2 : B36/S23",
@@ -183,18 +180,17 @@ void Main() {
 	const Array<int32> gameRuleOptions = { 1, 2, 3, 4, 5, 6, 7 };
 	int32 gameRule = gameRuleOptions[(int32)gameRuleIndex];
 
-
 	// World data
 	Grid<Cell> world(width + 2, height + 2, Cell{ 0,0 });
 
 	// For visualization
-	Image img{(uint32)width, (uint32)height, Black};
+	Image img{ (uint32)width, (uint32)height, Black };
 
 	// Texture
-	DynamicTexture texture{img};
+	DynamicTexture texture{ img };
 
-	// Time
-	Stopwatch stopwatch{StartImmediately::Yes};
+	// Control timer for play speed
+	Stopwatch stopwatch{ StartImmediately::Yes };
 
 	// Do auto play?
 	bool autoPlay = false;
@@ -209,6 +205,7 @@ void Main() {
 	SetTerminationTriggers(UserAction::CloseButtonClicked);
 	while (Update())
 	{
+		// Default width : 1280, height : 700
 		Resize(width * 10 + 280, (height >= 65 ? height * 10 : 650));
 
 		// When Button "Set Random" was pushed
@@ -256,7 +253,7 @@ void Main() {
 
 		// Edit cell on the field
 		if (!autoPlay && Rect{ 0, 0, width * 10 - 1, height * 10 - 1 }.mouseOver()) {
-			const Point MousePos = Cursor::Pos() / 10 + Point{1, 1};
+			const Point MousePos = Cursor::Pos() / 10 + Point{ 1, 1 };
 
 			if (MouseL.pressed()) {
 				world[MousePos].current = true;
@@ -284,7 +281,6 @@ void Main() {
 		for (auto i : step(height + 1))
 		{
 			Rect{ 0, i * 10, width * 10, 1 }.draw(ColorF{ 0.4 });
-			
 		}
 		for (auto i : step(width + 1)) {
 			Rect{ i * 10, 0, 1, height * 10 }.draw(ColorF{ 0.4 });
